cedfd4073f3c1034afcd0d2192a6bf6a4f2515a4
Vamseedhar <vamseedhar.reddyvari@gmail.com>
2017-06-14 14:51:13 -0500

    Buffer Evolution with time plotted for different policies

--- src/analyse.py
+++ src/analyse.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import itertools
 import numpy as np
 import pandas as pd
-# import seaborn as sns
-# sns.set(style="whitegrid")
+import seaborn as sns
+sns.set(style="whitegrid")
 
 FOLDER_NAME = r"../"
 
@@ -15,26 +15,43 @@ def read_no_pkts(filename):
         peer_evolution.append(value)
     return peer_evolution
 
-list1 = [ "group-suppression-US-2-lambda-1.9.txt", "group-suppression-US-2-lambda-2.5.txt", "group-suppression-US-2-lambda-4.0.txt","group-suppression-US-2-lambda-100.txt" ]
-labels1 =[ r"$U_s=2, \lambda=1.9$", r"$U_s=2, \lambda=2.5$", r"$U_s=2, \lambda=4.0$", r"$U_s=2, \lambda=100$"]
+BUFFEREVOL = True
+if(BUFFEREVOL):
+    names_array = list(["col"+str(x) for x in range(1,11)])
+    buffer_df = pd.read_csv(FOLDER_NAME+"Rarest-Us-2-lambda-4-distribution.txt",sep='\s+',names=names_array)
+    plt.figure()
+    plt.hold(True)
+    for elem in names_array:
+        plt.plot(pd.ewma(buffer_df[elem],halflife=10))
+    plt.ylabel("Bufer Marginal Distribution")
+    plt.savefig("buffer_evolution.pdf",dpi=300);
 
-list2 = ["Decline-Most-Popular-Us-2-lambda-1.9.txt" ,"Decline-Most-Popular-Us-2-lambda-4.txt" , "Decline-Most-Popular-Us-2-lambda-50.txt" ,"Decline-Most-Popular-Us-2-lambda-100.txt", ]
-labels2 =[ r"$U_s=2, \lambda=1.9$", r"$U_s=2, \lambda=4$", r"$U_s=2, \lambda=50$", r"$U_s=2, \lambda=100$"]
-peer_evolution = []
-for file_name in list2:
-    peer_evolution.append(pd.read_csv(FOLDER_NAME+file_name,'\n'))
+NOPKTS = False
+if (NOPKTS):
+    list1 = [ "group-suppression-US-2-lambda-1.9.txt", "group-suppression-US-2-lambda-2.5.txt", "group-suppression-US-2-lambda-4.0.txt","group-suppression-US-2-lambda-100.txt" ]
+    labels1 =[ r"$U_s=2, \lambda=1.9$", r"$U_s=2, \lambda=2.5$", r"$U_s=2, \lambda=4.0$", r"$U_s=2, \lambda=100$"]
 
-fig,((ax1,ax2),(ax3,ax4))= plt.subplots(2,2,figsize=(14,6))
-axes = [ax1,ax2,ax3,ax4]
-colors=['red','blue','green','black']
+    list3 = ["Decline-Most-Popular-Us-2-lambda-1.9.txt" ,"Decline-Most-Popular-Us-2-lambda-4.txt" , "Decline-Most-Popular-Us-2-lambda-50.txt" ,"Decline-Most-Popular-Us-2-lambda-100.txt", ]
+    labels3 =[ r"$U_s=2, \lambda=1.9$", r"$U_s=2, \lambda=4$", r"$U_s=2, \lambda=50$", r"$U_s=2, \lambda=100$"]
 
-for i in range(4):
-    axes[i].plot(peer_evolution[i].values,'-',color=colors[i],alpha=0.7,label=labels2[i])
-    ax = axes[i]
-    ax.grid(True)
-    ax.set_xlabel("Time")
-    ax.legend(loc=2)
-    ax.set_ylabel("Number of Peers")
+    list2 = ["Rarest-Us-2-lambda-1.9.txt" ,"Rarest-Us-2-lambda-4.txt" , "Rarest-Us-2-lambda-50.txt" ,"Rarest-Us-2-lambda-100.txt", ]
+    labels2 =[ r"$U_s=2, \lambda=1.9$", r"$U_s=2, \lambda=4$", r"$U_s=2, \lambda=50$", r"$U_s=2, \lambda=100$"]
 
-plt.suptitle('Decline Most Popular Pkt (m=10)',size=18)
-plt.savefig("Decline-Popular-Pkt.pdf",dpi=300)
+    peer_evolution = []
+    for file_name in list2:
+        peer_evolution.append(pd.read_csv(FOLDER_NAME+file_name,'\n'))
+
+    fig,((ax1,ax2),(ax3,ax4))= plt.subplots(2,2,figsize=(14,6))
+    axes = [ax1,ax2,ax3,ax4]
+    colors=['red','blue','green','black']
+
+    for i in range(4):
+        axes[i].plot(peer_evolution[i].values,'-',color=colors[i],alpha=0.7,label=labels2[i])
+        ax = axes[i]
+        ax.grid(True)
+        ax.set_xlabel("Time")
+        ax.legend(loc=2)
+        ax.set_ylabel("Number of Peers")
+
+    plt.suptitle('(m=10)',size=18)
+    plt.savefig("peer-evolution.pdf",dpi=300)
--- src/com/vamsee/p2p/no/streaming/P2PSimulation.java
+++ src/com/vamsee/p2p/no/streaming/P2PSimulation.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Random;
 
 import static java.lang.Math.max;
+import static java.lang.Math.min;
 import static java.lang.Math.pow;
 
 
@@ -25,8 +26,14 @@ public class P2PSimulation {
     private int NumberOfPeers; // Total peers in the system
     private ArrayList<Peer> ListPeers; // List of all current peers
     private int NumberOfPieces; // Buffer length
+    private boolean RANDOM;
+    private boolean RAREST;
+    private boolean GROUPSUPP;
+    private boolean DECLINE_POPULAR;
+    private boolean CHAIN_POLICY;
 
     PrintWriter writer;
+    PrintWriter writer_distribution;
 
     // Meta Variables
     /* - At any time sum of bufferDistributionCount should be equal to Number of Peers.
@@ -35,6 +42,7 @@ public class P2PSimulation {
     private int[] bufferDistributionCount; // 1x2^m array of number of peers that contain same pieces
     private HashMap<Integer,ArrayList<Peer> > PeerNumberOfPktsMap; // 1xm+1 Dictionary which maps the number of packets \
                                                         // to a list of all peers that contain that many number of packets.
+    private ArrayList<ArrayList<Peer> > PeerNumberOfPktsArray; // 1xm+1 Dictionary which maps the number of packets \
 
     // Utility Classes
     private Random rand; // Object for generating random numbers
@@ -47,6 +55,11 @@ public class P2PSimulation {
         this.lambda_p = lambda_p;
         this.Us = Us;
         this.lambda = lambda;
+        this.RANDOM = false;
+        this.RAREST = false;
+        this.GROUPSUPP = false;
+        this.DECLINE_POPULAR = false;
+        this.CHAIN_POLICY = false;
 
         //Initialize Peers
         this.NumberOfPeers = 5; //Lets start with 5 peers
@@ -58,7 +71,8 @@ public class P2PSimulation {
 
         // Utility Objects
         rand = new Random(); //initialize random
-        writer = new PrintWriter("Decline-Most-Popular-Us-2-lambda-4.txt", "UTF-8");
+        writer = new PrintWriter("Rarest-Us-2-lambda-4.txt", "UTF-8");
+        writer_distribution = new PrintWriter("Rarest-Us-2-lambda-4-distribution.txt", "UTF-8");
 
         // Meta-Variable initialization
         marginalPktDistributionCount = new int[NumberOfPieces];
@@ -74,6 +88,16 @@ public class P2PSimulation {
         for(int j = 0; j< NumberOfPeers; j++){
             PeerNumberOfPktsMap.get(0).add(ListPeers.get(j)); // All peers doesn't have any packets. So 0 packets will map to entire list
         }
+
+
+        PeerNumberOfPktsArray = new ArrayList<ArrayList<Peer>>();
+        for(int i = 0; i < NumberOfPieces +1; i++) {
+            PeerNumberOfPktsArray.add(new ArrayList<Peer>());
+        }
+        for(int j = 0; j< NumberOfPeers; j++){
+            PeerNumberOfPktsArray.get(0).add(ListPeers.get(j)); // All peers doesn't have any packets. So 0 packets will map to entire list
+        }
+
         // Sanity Check
         assertMetaVariables();
 
@@ -106,9 +130,30 @@ public class P2PSimulation {
         // Generate sojourn times for each poisson random variable and execute the event with least sojourn time
 
 
-        Ticks = (int) pow(10,6);
+        Ticks = 2*(int) pow(10,5);
         for(int t = 0; t< Ticks; t++){
-            double peerContactTime = expRand(NumberOfPeers *lambda_p);
+            RANDOM = true;
+//            CHAIN_POLICY = true;
+//            DECLINE_POPULAR = true;
+            if (t> pow(10,5) ){
+                RANDOM = false;
+                GROUPSUPP = false;
+                RAREST = false;
+                DECLINE_POPULAR = true;
+                CHAIN_POLICY = false;
+            }
+            double [] peersTicks = new double[NumberOfPeers];
+            double minTick = 100;
+            int minTickIdx = 0;
+            for(int i=0;i<NumberOfPeers;i++){
+                double newTick = expRand(lambda_p);
+                if (minTick > newTick){
+                    minTick = newTick;
+                    minTickIdx = i;
+                }
+            }
+
+            double peerContactTime = minTick;
             double seedContactTime= expRand(Us);
             double peerArrivalTime = expRand(lambda);
 
@@ -117,7 +162,7 @@ public class P2PSimulation {
                 arrivalEvent();
             }
             else if(peerContactTime<= peerArrivalTime && peerContactTime <= seedContactTime){
-                peer2PeerEvent();
+                peer2PeerEvent(minTickIdx);
                 if (NumberOfPeers ==0) {break;}
             }
             else if ( seedContactTime<=peerArrivalTime && seedContactTime<=peerContactTime){
@@ -126,23 +171,35 @@ public class P2PSimulation {
             }
         }
         writer.close();
+        writer_distribution.close();
 
         return 0;
     }
 
 
     /* Events */
-    private void peer2PeerEvent(){
+    private void peer2PeerEvent(int srcPeerIdx){
         // Peers Exchange packets
-        int srcPeerIdx= rand.nextInt(NumberOfPeers);
         Peer srcPeer = ListPeers.get(srcPeerIdx);
         int dstPeerIdx = rand.nextInt(NumberOfPeers);
         Peer dstPeer = ListPeers.get(dstPeerIdx);
-        // transfer random useful packet from src to dst
-//         int pktIdx = declineMostPopularChunk(dstPeer, srcPeerIdx);
-//         int pktIdx = peerRarestChunk(dstPeer, srcPeerIdx);
-//         int pktIdx = randomPeerChunk(dstPeer, srcPeerIdx);
-        int pktIdx = peerChunkGroupSuppression(dstPeer, srcPeerIdx);
+        if (RANDOM){
+            // transfer random useful packet from src to dst
+            int pktIdx = randomPeerChunk(dstPeer, srcPeerIdx);
+        }
+        else if (GROUPSUPP){
+            int pktIdx = peerChunkGroupSuppression(dstPeer, srcPeerIdx);
+        }
+        else if (RAREST){
+            int pktIdx = peerRarestChunk(dstPeer, srcPeerIdx);
+        }
+        else if (DECLINE_POPULAR){
+             int pktIdx = declineMostPopularChunk(dstPeer, srcPeerIdx);
+        }
+        else if (CHAIN_POLICY){
+
+            int pktIdx = peerChainPolicy(dstPeer, srcPeerIdx);
+        }
         assertMetaVariables();
         // Check if dst Peer has received all the packets. If yes then remove the peer from system
         boolean removed = removePeer(dstPeer);
@@ -151,17 +208,31 @@ public class P2PSimulation {
         Peer newPeer = new Peer();
         ListPeers.add(newPeer);
         NumberOfPeers = NumberOfPeers +1;
-        writer.println(NumberOfPeers);
         //update Distribution
         bufferDistributionCount[0] += 1;
         PeerNumberOfPktsMap.get(0).add(newPeer);
+        PeerNumberOfPktsArray.get(0).add(newPeer);
         assertMetaVariables();
+        writer.println(NumberOfPeers);
+        double []pdf = normalize();
+        for(int k=0;k< NumberOfPieces; k++){
+            String prob = String.format("%.3f", pdf[k] );
+            writer_distribution.print(prob+" ");
+        }
+        writer_distribution.println();
+    }
+    private double[] normalize(){
+        double[] pdf = new double[NumberOfPieces];
+        for(int i=0;i < NumberOfPieces;i++){
+            pdf[i] = (double) marginalPktDistributionCount[i]/NumberOfPeers;
+        }
+        return pdf;
+
     }
     private void seedTransferEvent(){
         // Seed gives a packet
         /* Peer Selection - Either random or Most Deprived*/
         boolean RANDOMPEER = true;
-        boolean DEPRIVEDPEER= false;
 
         int dstPeerIdx=0;
         Peer dstPeer = ListPeers.get(dstPeerIdx);
@@ -169,7 +240,7 @@ public class P2PSimulation {
             dstPeerIdx = rand.nextInt(NumberOfPeers);
             dstPeer = ListPeers.get(dstPeerIdx);
         }
-        else if (DEPRIVEDPEER){
+        else if (GROUPSUPP || CHAIN_POLICY){
             // Select a most deprived peer
             int k=0;
             for(k=0; k <NumberOfPieces+1; k++){
@@ -178,22 +249,38 @@ public class P2PSimulation {
                 }
             }
             ArrayList<Peer> deprivedList = PeerNumberOfPktsMap.get(k);
+//            ArrayList<Peer> deprivedList = PeerNumberOfPktsMap.get(k);
             int deprivedListIdx = rand.nextInt(deprivedList.size());
             dstPeer = deprivedList.get(deprivedListIdx);
         }
 
+        if (RANDOM || GROUPSUPP || CHAIN_POLICY) {
+            int pktIdx = seedRandomChunk(dstPeer);
+        }
+        else if (RAREST) {
+            int pktIdx = seedRarestChunk(dstPeer);
+        }
+        else if (DECLINE_POPULAR){
+            int pktIdx = seedDeclineMostPopularChunk(dstPeer);
+        }
         // Transfer a random useful packet from seed to dst
-        int pktIdx = seedRandomChunk(dstPeer);
-//        int pktIdx = seedDepriveMostPopularChunk(dstPeer);
         assertMetaVariables();
 
-        // Check if dst Peer has recieved all the packets. If yes then remove the peer from system
+        // Check if dst Peer has received all the packets. If yes then remove the peer from system
         boolean removed = removePeer(dstPeer );
         assertMetaVariables();
     }
 
     private boolean removePeer(Peer dstPeer){
         // Returns true if peer has all the packets and removed, else returns false
+        writer.println(NumberOfPeers);
+        double []pdf = normalize();
+        for(int k=0;k< NumberOfPieces; k++){
+            String prob = String.format("%.3f", pdf[k] );
+            writer_distribution.print(prob+" ");
+        }
+        writer_distribution.println();
+        // return value
         if(Arrays.equals(dstPeer.Buffer, Peer.FullBuffer)){
             // remove
             ListPeers.remove(dstPeer);
@@ -204,7 +291,7 @@ public class P2PSimulation {
             }
             bufferDistributionCount[toDecimal(Peer.FullBuffer)] = bufferDistributionCount[toDecimal(Peer.FullBuffer)] -1;
             PeerNumberOfPktsMap.get(NumberOfPieces).remove(dstPeer);
-            writer.println(NumberOfPeers);
+            PeerNumberOfPktsArray.get(NumberOfPieces).remove(dstPeer);
             assertMetaVariables();
             return true; // Peer is removed
         }
@@ -353,30 +440,17 @@ public class P2PSimulation {
     private int peerChainPolicy(Peer dstPeer, int srcIdx){
         /*
         - Here Chain policy is implemented
+        - A src will upload only if dst has more number of pacekts
         - A peer uploads to peer which has only
         */
         int [] dst = dstPeer.Buffer;
         int [] src = ListPeers.get(srcIdx).Buffer;
 
         // Determine largestbuffer
-        int largestBuffer = 0;
-        int largestValue = 0;
-        for(int i=0; i< bufferDistributionCount.length;i++){
-            int newvalue = bufferDistributionCount[i];
-            if (newvalue > largestValue){
-                largestValue = newvalue;
-                largestBuffer = i;
-            }
+        if (sumPackets(dst) < sumPackets(src) ){
+            return -1; // abandoning the transfer to avoid single club
         }
-        if(toDecimal(src) == largestBuffer){
-            if (sumPackets(dst) < sumPackets(src) ){
-                return -1; // abandoning the transfer to avoid single club
-            }
-        }
-
-
         ArrayList<Integer> usefulPktIdx = new ArrayList<Integer>();
-
         // Find list of useful packets
         for(int i=0; i< dst.length; i++){
             if (src[i] > dst[i]){
@@ -417,7 +491,7 @@ public class P2PSimulation {
         }
         if (usefulPktIdx.size() >0) {
             // Select the rarest packet
-            int rarestPktIdx = 0;
+            int rarestPktIdx = usefulPktIdx.get(0);
             int rarestPktCount = NumberOfPeers;
             for (int j = 0; j < usefulPktIdx.size(); j++) {
                 int count = marginalPktDistributionCount[usefulPktIdx.get(j)];
@@ -427,7 +501,6 @@ public class P2PSimulation {
                 }
 
             }
-//            rarestPktIdx = usefulPktIdx.get(0);
             dst[rarestPktIdx] = 1;
             // update distribution
             updateDistributions(dst,rarestPktIdx);
@@ -482,20 +555,29 @@ public class P2PSimulation {
                 usefulPktIdx.add(i);
             }
         }
-        // Select a random packet and transfer
-        if (usefulPktIdx.size() >0){
-            int randomIdx = rand.nextInt(usefulPktIdx.size());
-            int pktIdx = usefulPktIdx.get(randomIdx);
-            dst[pktIdx] = 1;
-            //update Distribution
-            updateDistributions(dst,pktIdx);
+        if (usefulPktIdx.size() >0) {
+            // Select the rarest packet
+            int rarestPktIdx = usefulPktIdx.get(0);
+            int rarestPktCount = NumberOfPeers;
+            for (int j = 0; j < usefulPktIdx.size(); j++) {
+                int count = marginalPktDistributionCount[usefulPktIdx.get(j)];
+                if (count < rarestPktCount) {
+                    rarestPktCount = count;
+                    rarestPktIdx = usefulPktIdx.get(j);
+                }
+
+            }
+//            rarestPktIdx = usefulPktIdx.get(0);
+            dst[rarestPktIdx] = 1;
+            // update distribution
+            updateDistributions(dst,rarestPktIdx);
             updateHashMap(dstPeer);
-            return pktIdx;
+            return rarestPktIdx;
         }
         return -1;
 
     }
-    private int seedDepriveMostPopularChunk(Peer dstPeer){
+    private int seedDeclineMostPopularChunk(Peer dstPeer){
         /*
         src contains all elements
         */
@@ -560,10 +642,16 @@ public class P2PSimulation {
 
     /* Metadata Update Function */
     private void updateHashMap(Peer dstPeer){
-        {
-            int[] tempBuffer = dstPeer.Buffer;
-            PeerNumberOfPktsMap.get(sumPackets(tempBuffer) - 1).remove(dstPeer);
-            PeerNumberOfPktsMap.get(sumPackets(tempBuffer)).add(dstPeer);
+        int[] tempBuffer = dstPeer.Buffer;
+        PeerNumberOfPktsMap.get(sumPackets(tempBuffer) - 1).remove(dstPeer);
+        PeerNumberOfPktsMap.get(sumPackets(tempBuffer)).add(dstPeer);
+
+
+        PeerNumberOfPktsArray.get(sumPackets(tempBuffer) - 1).remove(dstPeer);
+        PeerNumberOfPktsArray.get(sumPackets(tempBuffer)).add(dstPeer);
+
+        if(!assertMetaVariables()) {
+            System.out.println("Testing Asserstion");
         }
 
 
@@ -595,6 +683,9 @@ public class P2PSimulation {
     }
     private double expRand(double lambda) {
         // Return an exponential random varible with parameter lambda
+        if (lambda <=0){
+            return Math.pow(2,30);
+        }
         return  Math.log(1-rand.nextDouble())/(-lambda);
     }
 }
